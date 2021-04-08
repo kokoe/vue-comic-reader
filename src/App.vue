ccaae011470e6cfd5da2f51f24746d72f7ac8474
kokoe <kokoe@sonicmoov.com>
2021-04-08 13:04:41 +0900

    update: Update site layout

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app" class="l-wrapper">
+    <div class="hero mb-4">
+      <div class="hero-body has-text-centered">
+        <h1 class="title is-1">vue-comic-reader</h1>
+        <p class="subtitle">Comic Reader Component For Vue.js</p>
+      </div>
+    </div>
+    <ExampleStandard class="container" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue';
+import ExampleStandard from './components/ExampleStandard.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
+    ExampleStandard
   }
-})
+});
 </script>
 
 <style lang="scss">
-#app {
+@import '~bulma';
+
+.l-wrapper {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ExampleStandard.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <h2 class="title is-2">Standard</h2>
+    <VueComicReader />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import VueComicReader from './vue-comic-reader/index.vue';
+
+export default Vue.extend({
+  name: 'ExampleStandard',
+  components: {
+    VueComicReader
+  }
+});
+</script>
+
+<style lang="scss">
+</style>
