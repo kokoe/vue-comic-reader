b8230ccc61fff5bc48bdd1276b2d8cd72541d150
kokoe <kokoe@sonicmoov.com>
2021-04-08 12:14:26 +0900

    update: Change eslint to semistyle

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,14 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    'semi': ['error', 'always'],
+    'semi-spacing': ['error', {'after': true, 'before': false}],
+    'semi-style': ['error', 'last'],
+    'no-extra-semi': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error'
   },
   overrides: [
     {
