28bacc57655c5e7a8599badbe448fa8bbb37ab08
kokoe <kokoe@sonicmoov.com>
2021-04-25 02:24:28 +0900

    update: slot content

--- src/components/ExampleSlot.vue
+++ src/components/ExampleSlot.vue
@@ -10,15 +10,31 @@
       <template #header-brand>
         <h3>vue-comic-reader - Use slot</h3>
       </template>
-      <template #first-page="slotScope">
+      <template #first-page>
         <div data-show-menu="true" class="columns is-flex-direction-column is-justify-content-center is-align-items-center" style="height: 100%;">
-          <img data-show-menu="true" src="https://placekitten.com/g/200/200" alt="" style="border-radius: 50%">
-          <p data-show-menu="true" class="my-4 is-size-5">Cat stories. Page: {{ slotScope.totalPage }}</p>
+          <figure class="image">
+            <img data-show-menu="true" src="https://placekitten.com/g/200/200" alt="" class="is-rounded">
+          </figure>
+          <p data-show-menu="true" class="my-4 is-size-5">Kitten photos.</p>
           <button class="button is-primary" @click="onClickStart">Start</button>
         </div>
       </template>
       <template #last-page>
-        last page!!!
+        <div class="columns is-flex-direction-column is-justify-content-center is-align-items-center" style="height: 100%;">
+          <div class="card" style="width: 30%">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="http://placekitten.com/g/128/96" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content">
+                <p class="mb-2">See More?</p>
+                <p class="is-size-5"><a href="https://placekitten.com/" target="_blank">placekitten</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
     </VueComicReader>
   </section>
