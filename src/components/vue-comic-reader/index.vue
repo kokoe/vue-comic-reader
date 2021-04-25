be0d497c199f8bd41d870a8a3833896035be31c1
kokoe <kokoe@sonicmoov.com>
2021-04-26 02:36:30 +0900

    refactor: Migrate types to index.d.ts

--- src/components/vue-comic-reader/index.vue
+++ src/components/vue-comic-reader/index.vue
@@ -121,62 +121,11 @@ import VueSlider from 'vue-slider-component';
 import { genUniqKey } from '@/utility';
 import Icon from '@/components/icon/icon.vue';
 import { Value as VueSliderValue } from 'vue-slider-component/typings';
+import type { Direction, PageContent, Pages, PageSlotScope, MenuSlotScope, EmitChangeActivePage, DataType } from '@/index.d';
 
 Swiper.use([Navigation, Lazy, A11y]);
 
 const isDev = Vue.config.devtools;
-
-export type Direction = 'horizontal' | 'vertical';
-
-export type PageContent = {
-  key: number;
-  pageNumber: number;
-  pagesIndex: number | null;
-  src: string | null;
-  slot: 'first-page' | 'last-page' | 'page';
-}
-
-export type Page = PageContent[];
-
-export type Pages = Page[];
-
-export type PageSlotScope = {
-  slideIndex: number;
-  pageContent: PageContent;
-  type: 'odd' | 'even';
-  spread: boolean;
-  totalPage: number;
-}
-
-export type MenuSlotScope = {
-  direction: Direction;
-  activePage: number;
-  totalPage: number;
-}
-
-export type EmitChangeActivePage = {
-  activePage: number;
-  isLastPage: boolean;
-}
-
-type DataType = {
-  swiper: Swiper | null;
-  direction: Direction;
-  activeSlideIndex: number;
-  isShowMenu: boolean;
-  transitioning: boolean;
-  hasFirstSlot: boolean;
-  hasLastSlot: boolean;
-  noticeDirection: {
-    isShow: boolean;
-    displayTime: number;
-    timerId_: number;
-  },
-  mq: MediaQueryList | null;
-  spread_: boolean;
-  domRefresh_: boolean;
-  initializedActiveSlideIndex_: boolean;
-}
 
 export default Vue.extend({
   name: 'VueComicReader',
