bfcce827ca391dd601aec144fb157600b6582c23
Saeko Onishi <lettuce@lettuce.sakura.ne.jp>
2024-02-23 22:54:36 +0900

    chore(vite): Setting library mode

--- packages/vue-comic-reader/vite.config.ts
+++ packages/vue-comic-reader/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url';
-
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
@@ -11,5 +11,29 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  build: {},
+  build: {
+    /**
+     * https://vitejs.dev/guide/build.html#library-mode
+     */
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'umd'],
+      name: 'vue-comic-reader',
+      // the proper extensions will be added
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 });
