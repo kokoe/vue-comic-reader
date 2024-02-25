ce682f873fd5d8512bc8eebf3a6250c0c33e6769
Saeko Onishi <lettuce@lettuce.sakura.ne.jp>
2024-02-25 23:50:18 +0900

    chore(storybook): Add tailwind style

--- packages/vue-comic-reader/.storybook/preview.ts
+++ packages/vue-comic-reader/.storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/vue3';
 
+import '@/assets/css/index.css';
+
 const preview: Preview = {
   // parameters: {
   //   actions: { argTypesRegex: "^on[A-Z].*" },
